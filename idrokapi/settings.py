5916f233b1e4a1a97dc2a4188c9f8baf8a1d6fe0
Ikromjon <80442693+Ikromjon0102@users.noreply.github.com>
2023-10-15 02:33:31 +0500

    allow

--- idrokapi/settings.py
+++ idrokapi/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'idrokapi.urls'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
